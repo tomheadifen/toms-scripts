298de56a4ab81c9631371017c46596f1eb6dcbe7
Thomas Headifen <thomas.headifen@gmail.com>
2026-04-05 16:57:57 -0400

    updating nvim config now does it via rsync to stop files from existing when they shouldnt

--- update-nvim.sh
+++ update-nvim.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p ~/.config/nvim
 
-cp -r "$SCRIPT_DIR/nvim"/. ~/.config/nvim/
+rsync -a --delete "$SCRIPT_DIR/nvim/" ~/.config/nvim/
 
-echo "Copied nvim config to ~/.config/nvim/"
+echo "Synced nvim config to ~/.config/nvim/"
